59757f2972a1634600e1b9267407abb6639fb5c1
v r a c b <165052649+valexyy@users.noreply.github.com>
2025-09-14 00:19:40 -0300

    Subiendo correciones y estilos, lo hago recen ahora por que me dieron el alta

--- registro de mascotas/js/eliminar.js
+++
@@ -1,8 +0,0 @@
-//Caro
-function eliminarMascota(indice) {
-    window.mascotas.splice(indice, 1);
-    mostrarMascotas(); // Llamamos a la función mostrar para refrescar la lista
-}
-
-// Exportar globalmente
-window.eliminarMascota = eliminarMascota;
